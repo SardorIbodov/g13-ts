6a16394a560f96ddd94425559fed705ed71f50a1
Sardor <sardoribodov07@gmail.com>
2023-12-05 09:49:35 +0500

    1. Typescript ga kirish

---
+++ 1/index.js
@@ -0,0 +1,10 @@
+"use strict";
+// console.log(12);
+// console.log(21);
+// console.log(121);
+// console.log(212);
+// let a = 12;
+// a = "webbrain"
+// a = {};
+// a = 21;
+// console.log(10 + true === 11);
---
+++ 1/index.ts
@@ -0,0 +1,14 @@
+// console.log(12);
+// console.log(21);
+// console.log(121);
+// console.log(212);
+
+// let a = 12;
+// a = "webbrain"
+// a = {};
+// a = 21;
+
+// console.log(10 + true === 11);
+
+
+
