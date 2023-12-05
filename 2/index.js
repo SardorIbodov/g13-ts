071769354619d6a8e0ac18174ab57532435efd38
Sardor <sardoribodov07@gmail.com>
2023-12-05 09:49:51 +0500

    2. Basics | Data-Types

---
+++ 2/index.js
@@ -0,0 +1,21 @@
+"use strict";
+//! Declaring a variable
+//! let / const / var variableName: data-type = value
+//? number
+// let a: number = 21;
+//? string
+// let a: string = "web";
+//? boolean
+// let a: boolean = true;
+// a = false;
+//? null
+// let a: null = null;
+//? undefined
+// let a: undefined = undefined;
+//? object
+// let a: {name: string, age?: number} = {name: "web"};
+// a.age = 2020;
+// console.log(a);
+//? any
+// let a: any = "web";
+// a = 21;
