e7be85ac807b2dfb47094e01b9556dff91dbca7b
Sardor <sardoribodov07@gmail.com>
2023-12-07 10:24:07 +0500

    array | tuple | enum

---
+++ 5/index.ts
@@ -0,0 +1,29 @@
+//!
+// let evens = [2, 4, 6, 8, 10];
+// evens = ["ikki", "to'rt"]
+//!
+// let evens: number[] = [2, 4, 6, 8];
+// evens = [3, 5, 7, 9]
+//! union + array
+// let evens: (number| string)[] = [2, "to'rt"]
+
+//!
+// let a: Array<number | string> = [1, 2, 3, "4"];
+// a = ["st"];
+
+//! tuple
+// let user: [number, string] = [1, "web"];
+
+//! Enum => enumeration
+// enum sizes {
+//   s = 5,
+//   m = 1,
+//   l = 9,
+//   xl,
+// }
+// console.log(sizes.s);
+// console.log(sizes.m);
+// console.log(sizes.l);
+// console.log(sizes.xl);
+
+// console.log(sizes[2]);
