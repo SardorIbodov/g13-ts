6eefb8047f4bb20845721568e406dc4bbd3c8259
Sardor <sardoribodov07@gmail.com>
2023-12-12 10:48:22 +0500

    Generics

---
+++ 12/index.js
@@ -0,0 +1,16 @@
+"use strict";
+//! Syntax of Generics
+// type A<T> = T;
+// type B = A<string>;
+// type C = A<boolean>;
+//! Array with Generics
+// type myArray<T> = T;
+// let arr1: myArray<string[]> = ["1", "2", "3"];
+// let arr2: myArray<number[]> = [1, 2, 3];
+//! Function with Generics
+// function echo<T>(x: T): T {
+// 	return x;
+// }
+// console.log(echo("a"));
+// console.log(echo(1));
+// console.log(echo(true));
